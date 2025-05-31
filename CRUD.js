59a8ddd808bcc06146f1c233ec018ed49a6ce379
saint-zb <aabdulhazeeb@gmail.com>
2025-05-31 05:49:51 +0100

    Add files via upload

---
+++ CRUD.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World from Node.js!');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
